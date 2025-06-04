a596133ed8fcf807aea28a0e85f9386e829441d9
eugenechua <eugene.chua.mail@gmail.com>
2025-06-04 16:42:01 +0800

    fix: changed it with tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    return (
+      <div className="h-screen flex items-center justify-center bg-slate-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          ✅ Tailwind is working!
+        </h1>
+      </div>
+    );
+  }
+  
+  export default App;  
