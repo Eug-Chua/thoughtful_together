ded16f58cfcb5caa641a8df3a89c0dd07004d917
eugenechua <eugene.chua.mail@gmail.com>
2025-06-09 23:43:51 +0800

    fix: added fonts and updated color code

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,12 +4,15 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Satoshi', 'Inter', 'sans-serif'],
+        fun: ['Comic Neue', 'Quicksand', 'Nunito', 'cursive'], // Add this line
+        playful: ['Fredoka One', 'Righteous', 'Comfortaa', 'cursive'],
+        retro: ['Press Start 2P', 'Orbitron', 'monospace'],
       },
       colors: {
         tealLight:  '#9cdee2',
         tealMid:    '#46a4ab',
         tealBase:   '#09747f',
-        tealDark:   '#298389',
+        tealDark:   '#086c77',
       },
       animation: {
         'fade-in': 'fadeIn 0.9s ease-in-out',
