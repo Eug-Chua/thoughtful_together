7483718c9c46d3371f56b89ab818b42e98771b76
eugenechua <eugene.chua.mail@gmail.com>
2025-06-09 12:16:58 +0800

    fix: added color palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
       fontFamily: {
         sans: ['Satoshi', 'Inter', 'sans-serif'],
       },
+      colors: {
+        tealLight:  '#9cdee2',
+        tealMid:    '#46a4ab',
+        tealBase:   '#09747f',
+        tealDark:   '#298389',
+      },
       animation: {
         'fade-in': 'fadeIn 0.9s ease-in-out',
       },
